5d6565ca57a9227538d0d4d2f95948215b91a58a
Soumyaranjan <soumyaranjansahoo97292@gmail.com>
2026-04-24 12:51:49 +0530

    Update assign7.py

--- assign7.py
+++ assign7.py
@@ -51,3 +51,35 @@ print("Title case:", s.title())
 print("Replace spaces with dash:", s.replace(" ", "-"))
 print("Number of 'a':", s.count('a'))
 print("Reversed string:", s[::-1])
+
+
+
+
+# 2. Tuple with user input
+
+lst = []
+
+for i in range(6):
+    x = int(input("Enter number: "))
+    lst.append(x)
+
+t = tuple(lst)
+
+print(t)
+print(len(t))
+print(max(t), min(t))
+
+print(45 in t)
+
+lst = list(t)
+lst[2] = int(input("Enter new 3rd element: "))
+t = tuple(lst)
+print(t)
+
+lst = list(t)
+lst.pop(3)
+t = tuple(lst)
+print(t)
+
+for x in t:
+    print(x, x**3)
