f87a6f5fb933d52bcdeec046afbab0bc91e884b2
Soumyaranjan <soumyaranjansahoo97292@gmail.com>
2026-05-19 20:17:17 +0530

    Update assign7.py

--- assign7.py
+++ assign7.py
@@ -1,49 +1,4 @@
 
-# 6. Method overriding
-class Parent2:
-    def show(self):
-        print("Parent method")
-
-class Child2(Parent2):
-    def show(self):
-        print("Child method")
-
-c2 = Child2()
-c2.show()
-
-
-# 7. Multiple inheritance
-class Father:
-    def skill1(self):
-        print("Father skill")
-
-class Mother:
-    def skill2(self):
-        print("Mother skill")
-
-class Child3(Father, Mother):
-    pass
-
-c3 = Child3()
-c3.skill1()
-c3.skill2()
-
-
-# 8. Class variable and instance variable
-class Student3:
-    college = "TAC"   # class variable
-
-    def __init__(self, name):
-        self.name = name   # instance variable
-
-s3 = Student3("Soumya")
-s4 = Student3("Rahul")
-
-print(s3.name, s3.college)
-print(s4.name, s4.college)
-
-
-
 # Assignment-10
 
 # 1. Employee class
